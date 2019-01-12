422f49df1e72da9c7ca9faab4719e7b4db2df7ce
unknown <you@example.com>
2019-01-12 20:58:33 +0800

    add products

---
+++ products.py
@@ -0,0 +1,13 @@
+#記帳程式
+products = [] #建立大清單
+while True:
+    name = input('請輸入商品名稱: ')
+    if name == 'q':
+        break
+    price = input('請輸入價格: ')
+    p = [] #建立小清單
+    # p.append(name) 
+    # p.append(price)
+    p = [name, price]
+    products.append(p)
+print(products)
