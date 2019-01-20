2f2524005cd6464f467b8607304d3abc949a1550
unknown <you@example.com>
2019-01-20 16:05:23 +0800

    add check file

--- products.py
+++ products.py
@@ -1,12 +1,19 @@
+import os # 載入模組
+
 products = [] #建立大清單
 # 讀取檔案
-with open ('products.csv', 'r', encoding='utf-8') as f:
-    for line in f:
-        if '商品,價格' in line:
-            continue #跳到下一個迴圈
-        name, price = line.strip().split(',') #去掉換行符號\n 在切割字串
-        products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'): # 在同樣的資料夾檢查檔案 
+    print('找到檔案')
+    with open ('products.csv', 'r', encoding='utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue #跳到下一個迴圈
+            name, price = line.strip().split(',') #去掉換行符號\n 在切割字串
+            products.append([name, price])
+    print(products)
+else:
+    print('找不到檔案.........')
+
 # 讓使用者輸入
 while True:
     name = input('請輸入商品名稱: ')
